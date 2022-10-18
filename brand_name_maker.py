0b0959a63aedeb663d447be2e32e15c872b7cbb3
Görkem AVCI <110812200+gorkemvci@users.noreply.github.com>
2022-10-18 17:56:07 +0300

    Add files via upload

---
+++ brand_name_maker.py
@@ -0,0 +1,5 @@
+print("Brand_Name Maker")
+print("***What is your Brand Name ? Game***")
+color=input("What is your favorite color? ")
+animal=input("What is your favorite animal? ")
+print("Your brands name is "+color+animal)
